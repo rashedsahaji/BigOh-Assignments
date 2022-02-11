c96cd5c6542216bf0e87bfee4397cc59686cf244
rashedsahaji <asifrashed2@gmail.com>
2022-02-11 18:09:11 +0530

    Day 5: Add thirdQuestion

---
+++ Day 5/thirdQuestion.js
@@ -0,0 +1,20 @@
+function search() {
+    let matrix = [
+        [1,3,5,7],
+        [10,11,16,20],
+        [23,30,34,60]
+    ];
+    
+    let target = 20;
+    
+    for(let i=0; i<matrix.length;i++){
+        for(let j=0; j<matrix[i].length;j++){
+            if(matrix[i][j]===target){
+                return 'Found';
+            }
+        }
+    }
+    return 'Not Found';
+}
+
+console.log(search());
