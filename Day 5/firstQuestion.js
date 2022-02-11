00f630a56fbeb4f58af0473a416bfa0e11bcacac
rashedsahaji <asifrashed2@gmail.com>
2022-02-11 16:16:47 +0530

    Day 5: Add first question

--- Day 5/firstQuestion.js
+++ Day 5/firstQuestion.js
@@ -1,11 +1,79 @@
-let arr = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-];
+function clockWise(){
+    let arr = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9]
+    ];
+    
+    let left = 0;
+    let top = 0;
+    let right = 3-1;
+    let bottom = 3-1;
 
-for(let i=0;i<arr.length;i++){
-    for(let j=0;arr[i].length;j++){
-        
+    let temp = [];
+    
+    while(top<=bottom && left<=right){
+        for(let i=left; i<=right;i++){
+            temp.push(arr[top][i]);
+        }
+        top++;
+        for(let i=top;i<=bottom;i++){
+            temp.push(arr[i][right]);
+        }
+        right--;
+        if(left<=right){
+            for(let i=right;i>=left;i--){
+                temp.push(arr[bottom][i]);
+            }
+            bottom--;
+        }
+        if(top<=bottom){
+            for(let i=bottom;i>=top;i--){
+                temp.push(arr[i][left])
+            }
+            left++;
+        }
     }
+    return temp;
 }
+console.log(clockWise());
+console.log('-------------------')
+function antiClockWise(){
+    let arr = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9]
+    ];
+    
+    let left = 0;
+    let top = 0;
+    let right = 3-1;
+    let bottom = 3-1;
+
+    let temp = [];
+
+    while(top<=bottom && left<=right){
+        for(let i=right; i>=left;i--){
+            temp.push(arr[top][i]);
+        }
+        top++;
+        for(let i=top;i<=bottom;i++){
+            temp.push(arr[i][left]);
+        }
+        left++;
+        if(left<=right){
+            for(let i=left;i<=right;i++){
+                temp.push(arr[bottom][i]);
+            }
+            bottom--;
+        }
+        if(top<=bottom){
+            for(let i=bottom;i>=top;i--){
+                temp.push(arr[i][right])
+            }
+            right--;
+        }
+    }
+    return temp;
+}
+console.log(antiClockWise());
